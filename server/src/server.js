c8de0e2a322bc29e8a60d007bc42967af9000c72
Florent Béjina <florent@bejina.fr>
2019-08-23 09:42:21 +0100

    basic express server setup

--- server/src/server.js
+++ server/src/server.js
@@ -1,1 +1,30 @@
-console.log('hello world');
+import express from 'express';
+import morgan from 'morgan';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+
+const APP_PORT = 3000;
+
+const App = express();
+
+// Body Parser setup
+App.use(bodyParser.urlencoded({
+  extended: true,
+}));
+App.use(bodyParser.json());
+
+// Cookie Parser setup
+App.use(cookieParser());
+
+// Morgan setup
+App.use(morgan('dev'));
+
+// CORS setup
+App.use(cors());
+
+App.use('/', (req, res) => res.send('hello world'));
+
+App.listen(APP_PORT, () => {
+  console.log(`Server is listening on port ${APP_PORT}!`);
+});
