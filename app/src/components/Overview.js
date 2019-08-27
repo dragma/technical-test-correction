9ffd19a20364ac1a787368090ca4660b12d36299
Florent Béjina <florent@bejina.fr>
2019-08-27 10:18:35 +0100

    add some game details on overview

--- app/src/components/Overview.js
+++ app/src/components/Overview.js
@@ -1,9 +1,34 @@
 import React from'react';
+import { Text } from 'react-native-elements';
 
 import Turn from './Turn';
 
 export default ({ game }) => !!game && (
   <>
+    <Text style={{
+      textAlign: 'center',
+      fontStyle: 'italic',
+      fontWeight: 'bold',
+    }}>
+      "{game.sentence}"
+    </Text>
+    <Text style={{
+      textAlign: 'center',
+    }}>
+      devient
+    </Text>
+    <Text style={{
+      textAlign: 'center',
+      fontStyle: 'italic',
+      fontWeight: 'bold',
+    }}>
+      "{game.final_sentence}"
+    </Text>
+    <Text style={{
+      textAlign: 'center',
+    }}>
+      Note globale : {Math.ceil(game.final_note * 200) / 10}/20
+    </Text>
     {game.turns.map((turn, key) => (
       <Turn
         key={turn._id}
